b81601a64fc45b26a5a277c9d203ab921bdb7cee
cedricrgt <128705301+cedricrgt@users.noreply.github.com>
2025-05-15 13:21:29 +0200

    create  the entities resources for vote, challenge and participation with crud endpoints

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { ChallengeModule } from './challenge/challenge.module';
+import { ParticipationModule } from './participation/participation.module';
+import { VoteModule } from './vote/vote.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ChallengeModule, ParticipationModule, VoteModule],
   controllers: [AppController],
   providers: [AppService],
 })
