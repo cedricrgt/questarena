9d850b3c3a00967a33e5a4d5fd90ee0f214dc2ac
cedricrgt <128705301+cedricrgt@users.noreply.github.com>
2025-06-03 14:19:56 +0200

    eat(home): add trending challenges section - Add challenges game images - Add section title with proper spacing - Set up container width and margins for better layout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,40 @@
 import Button from "./components/button/button";
 import Hero from "./components/hero/Hero";
+import ChallengeCard from "./components/challengeCard/challengeCard";
+
+const challenges = [
+  {
+    imageSrc: "/images/home/voidbreaker.webp",
+    game: "Voidbreaker",
+    title: "Speedrun Boss Rush",
+    participants: 50,
+  },
+  {
+    imageSrc: "/images/home/mythra.webp",
+    game: "Myhra",
+    title: "No Hit Run",
+    participants: 32,
+  },
+  {
+    imageSrc: "/images/home/ashes-cover.webp",
+    game: "Ashe",
+    title: "Score Max",
+    participants: 87,
+  },
+];
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <section className="w-4/5 mx-auto  my-11">
+        <h2 className="text-xl font-bold mb-4">Défis tendances</h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 justify-center gap-6">
+          {challenges.map((challenge, i) => (
+            <ChallengeCard key={i} {...challenge} />
+          ))}
+        </div>
+      </section>
+    </>
+  );
 }
