90fddfc27e5dea1372e9582f26cc49d54d931a3e
Lauréanne Drozdz <96056559+LaureanneDrozdz@users.noreply.github.com>
2025-06-03 11:37:34 +0200

    feat: add ParticipationCard component with video embedding

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,5 @@
 import ChallengeCard from './components/challengeCard/challengeCard';
+import ParticipationCard from './components/participationCard/participationCard';
 
 const challenges = [
   {
@@ -21,6 +22,30 @@ const challenges = [
   },
 ];
 
+  const fakeParticipations = [
+  {
+    link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+    title: 'Super combo de ouf 🔥',
+    nbVotes: 42,
+    challenge: 'Défi Combo Parfait',
+    userId: 2
+  },
+  {
+    link: 'https://youtu.be/3JZ_D3ELwOQ',
+    title: 'Victoire en 10 secondes',
+    nbVotes: 15,
+    challenge: 'Speedrun Éclair',
+    userId: 1
+  },
+  {
+    link: 'https://www.youtube.com/watch?v=oHg5SJYRHA0',
+    title: 'Clutch incroyable à la dernière seconde !',
+    nbVotes: 88,
+    challenge: 'Sauvetage Inespéré',
+    userId: 3
+  },
+];
+
 export default function Home() {
   return (
     <section>
@@ -30,6 +55,22 @@ export default function Home() {
           <ChallengeCard key={i} {...challenge} />
         ))}
       </div>
+
+      <h2 className="text-xl font-bold mb-4 mt-10">Les participations récentes</h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 ">
+        {fakeParticipations.map((item, index) => (
+        <ParticipationCard
+          key={index}
+          link={item.link}
+          title={item.title}
+          nbVotes={item.nbVotes}
+          challenge={item.challenge}
+          userId={item.userId}
+        />
+      ))}
+      </div>     
     </section>
+
+  
   );
 }
