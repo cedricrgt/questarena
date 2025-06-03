0504ef08350537c5e1c78513b6b9ee5eb6e25f4e
cedricrgt <128705301+cedricrgt@users.noreply.github.com>
2025-06-03 10:15:42 +0200

    add the title and text fonts

--- frontend/src/app/components/hero/Hero.tsx
+++ frontend/src/app/components/hero/Hero.tsx
@@ -2,7 +2,7 @@ import Button from "../button/button";
 
 const Hero = () => {
   return (
-    <section className="w-full h-[75vh] bg-[url(/hero/hero.webp)] bg-no-repeat bg-cover bg-center md:bg-left grid grid-cols-1 md:grid-cols-2 items-center justify-center relative">
+    <section className="w-full h-[50vh] bg-[url(/hero/hero.webp)] bg-no-repeat bg-cover bg-center md:bg-left grid grid-cols-1 md:grid-cols-2 items-center justify-center relative">
       <div className="span-col-1 items-center justify-center w-4/5 md:w-full max-w-[90rem] mx-auto px-4 relative z-10 bg-secondary/25 backdrop-blur-sm py-6 ">
         <h1 className="text-3xl text-center pb-2 text-shadow-lg">
           Relevez le défi !
@@ -12,7 +12,7 @@ const Hero = () => {
         <Button
           label="Créer un défi"
           variant="cta"
-          className="rounded-2xl px-11 my-4"
+          className="rounded-2xl px-11 my-4 self-center"
         />
       </div>
     </section>
