67a76823f10f18f22650a97e9b6b2671b332bb38
cedricrgt <128705301+cedricrgt@users.noreply.github.com>
2025-07-15 11:52:03 +0200

    add port 3000 to NEXT_PUBLIC_API_URL

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'http://cedricrgt45-server.eddi.cloud:5173',
+      'http://cedricrgt45-server.eddi.cloud',
+    ],
     credentials: true,
   });
 
