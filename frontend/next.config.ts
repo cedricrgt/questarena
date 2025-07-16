9f73500dcadeac5cfa841caea774004ce2f4f677
cedricrgt <128705301+cedricrgt@users.noreply.github.com>
2025-07-16 10:52:48 +0200

    Configure next for githubpages

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
+const isGithubPages =
+  process.env.GITHUB_ACTIONS || process.env.DEPLOY_TARGET === "GH_PAGES";
+
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: isGithubPages ? "/project-gamer-challenges" : "",
+  assetPrefix: isGithubPages ? "/project-gamer-challenges/" : "",
   images: {
     domains: [
       "randomuser.me",
