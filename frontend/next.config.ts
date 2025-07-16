6cbe27b8dd235bfc9e275f49b68d960daa80f58b
cedricrgt <128705301+cedricrgt@users.noreply.github.com>
2025-07-16 10:42:49 +0200

    Create workflow for frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -8,7 +8,8 @@ const nextConfig: NextConfig = {
       "www.worldhistory.org",
       "example.com",
       "placehold.co",
-      "rawg.io"
+      "rawg.io",
+      "media.rawg.io",
     ],
   },
 };
